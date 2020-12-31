3a35c84aef81102d6190c7cef737b769b3691c88
James Roeder <jmaroeder@gmail.com>
2020-12-31 14:27:57 -0500

    feat: day 13 (part 2)

--- day13/main.py
+++ day13/main.py
@@ -1,7 +1,9 @@
 import logging
 import textwrap
+from functools import reduce
 from pathlib import Path
 from typing import Any
+from typing import Sequence
 from typing import Set
 from typing import Tuple
 
@@ -10,19 +12,18 @@ import advent
 LOG = logging.getLogger(__name__)
 
 
-def parse_input(s: str) -> Tuple[int, Set[int]]:
+def parse_input(s: str) -> Tuple[int, Sequence[int], Sequence[int]]:
     lines = s.splitlines()
     if len(lines) != 2:
         raise Exception(f"Unexpected # of lines of input ({len(lines)})")
-    return int(lines[0]), {int(bus_id) for bus_id in lines[1].split(",") if bus_id != "x"}
+    return int(lines[0]), [int(bus_id) for bus_id in lines[1].split(",") if bus_id != "x"], [idx for idx, bus_id in enumerate(lines[1].split(",")) if bus_id != "x"]
 
 
 def main(s: str) -> Tuple[Any, Any]:
     part1 = None
     part2 = None
 
-    bus_ids: str
-    earliest_timestamp, bus_ids = parse_input(s)
+    earliest_timestamp, bus_ids, offsets = parse_input(s)
 
     current_timestamp = earliest_timestamp
     while True:
@@ -34,7 +35,31 @@ def main(s: str) -> Tuple[Any, Any]:
             break
         current_timestamp += 1
 
+    LOG.debug(f"offsets: {offsets}")
+    part2 = chinese_remainder(bus_ids, [-offset for offset in offsets])
+
     return part1, part2
+
+
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a*b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a%b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1
 
 
 SAMPLE_INPUT = textwrap.dedent(
